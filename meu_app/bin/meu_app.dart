ed93cb6f086ef4484c68346645f51d77fef73f22
Leozera2k <leofsousa98@gmail.com>
2024-11-12 14:40:04 -0300

    Update meu_app.dart

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,5 +1,14 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
+  int numero1 = 10;
+  var numero2 = 11;
+
+  print("Retorna verdadeiro se e somente se esse inteiro for par");
+  print(numero1.isEven);
+  print(numero2.isEven);
+
+  print("Retorna verdadeiro se e somente se esse inteiro for impar");
+  print(numero1.isOdd);
+  print(numero2.isOdd);
 }
