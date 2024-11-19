cb3d57b98637d68d7b2a6d86976657b1959b4c9a
Leozera2k <leofsousa98@gmail.com>
2024-11-19 14:19:15 -0300

    Update meu_app.dart

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,18 +1,12 @@
 import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
-  double numero1 = 10.1;
-  var numero2 = 11.1;
 
-  print("Remove ponto flutuante");
-  print(numero1.truncate());
-  print(numero2.truncate());
-
-  print("Converte para inteiro: ");
-  print(numero1.toInt());
-  print(numero2.toInt());
-
-  print("Arredonda para cima!");
-  print(numero1.ceil());
-  print(numero2.ceil());
+  var prova1 = 9;
+  var prova2 = 5;
+  var media = (prova1 + prova2) / 2;
+  print(media);
+  print(media >= 7);
+  print(prova1 == prova2);
+  
 }
