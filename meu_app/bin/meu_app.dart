55b83fc60c717207775743eac121efde823aedea
Leozera2k <leofsousa98@gmail.com>
2024-11-19 14:29:12 -0300

    Update meu_app.dart

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -2,11 +2,15 @@ import 'package:meu_app/meu_app.dart' as meu_app;
 
 void main(List<String> arguments) {
 
-  var prova1 = 9;
+  var prova1 = 8;
   var prova2 = 5;
   var media = (prova1 + prova2) / 2;
-  print(media);
-  print(media >= 7);
-  print(prova1 == prova2);
   
+  if (media >= 7) {
+    print("O Aluno foi APROVADO! Nota final: $media");
+  } else if(media >= 5){
+print("O Aluno está de recuperação! Nota: $media");
+    } else {
+        print("O Aluno foi REPROVADO! Nota final: $media");
+    }
 }
