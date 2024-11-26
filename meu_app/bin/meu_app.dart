281f9740bbd62f54c98ac1dcccc803f956e8fead
Leozera2k <leofsousa98@gmail.com>
2024-11-26 11:18:38 -0300

    cálculo fatorial

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,3 +1,10 @@
 void main(List<String> arguments) {
-    
+    print( fatorial(3));
+}
+
+int fatorial(int numero){
+    if(numero == 1){
+        return 1;
+    }
+    return numero * fatorial(numero - 1);
 }
