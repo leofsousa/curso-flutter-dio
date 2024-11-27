5902e5f6de61f681861675ad2ca7a3c9961e2156
Leozera2k <leofsousa98@gmail.com>
2024-11-27 10:38:05 -0300

    Update meu_app.dart

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,7 +1,5 @@
 void main(List<String> arguments) {
-    print(soma(150, 200));
+    
+    
 }
 
-void printData(String texto) => print (texto);
-    
-int soma(int number1, int number2) => number1 + number2;
